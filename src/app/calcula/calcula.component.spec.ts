173c5c53369307290eecaa68eb404e6e8b02a4fe
Emely Melo <emely.melo1036@gmail.com>
2018-07-03 20:24:51 -0300

    + testes

--- src/app/calcula/calcula.component.spec.ts
+++ src/app/calcula/calcula.component.spec.ts
@@ -9,14 +9,24 @@ describe('CalculaComponent', () => {
     component = new CalculaComponent;
     expect(component).toBeTruthy();
   });
-  it("verifica se 1 + 1 = 2", ()=>{
+  it("verifica o método de soma", ()=>{
     expect(component.soma(1,1)).toEqual(2);
+    expect(component.soma(2000,4738)).toEqual(6738);
+    expect(component.soma(30,0.2)).toEqual(30.2);
   });
-  it("verifica se 2 - 1 = 1", ()=>{
+  it("verifica o método de subtração", ()=>{
+    expect(component.subtracao(2,1)).toEqual(1);
+    expect(component.subtracao(38736,36)).toEqual(38700);
+  });
+  it("verifica a exceção do método de subtração", ()=>{
+    expect(component.subtracao(2,1)).toEqual(1);
     expect(component.subtracao(2,1)).toEqual(1);
   });
   it("verifica se 2 * 2 = 4", ()=>{
     expect(component.multiplicacao(2,2)).toEqual(4);
+    expect(component.multiplicacao(200,5)).toEqual(1000);
+    expect(component.multiplicacao(73635,0)).toEqual(0);
+
   });
   /*it("verifica se 8 / 4 = 2", ()=>{
     expect(component.divisao(8,4)).toEqual(2);
