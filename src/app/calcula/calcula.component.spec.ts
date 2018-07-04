9ecc156fc22f4067748f2d6345771d06d8bdb7cf
Emely Melo <emely.melo1036@gmail.com>
2018-07-03 21:11:53 -0300

    + testes

--- src/app/calcula/calcula.component.spec.ts
+++ src/app/calcula/calcula.component.spec.ts
@@ -13,32 +13,28 @@ describe('CalculaComponent', () => {
     expect(component.soma(1,1)).not.toEqual(1);
     expect(component.soma(2000,4738)).toEqual(6738);
     expect(component.soma(30,0.2)).toEqual(30.2);
+    expect(component.soma(0,-3)).not.toBeNull;
+    expect(component.subtracao(9.25,1)).not.toEqual(7);
   });
   it("verifica o método de subtração", ()=>{
     expect(component.subtracao(2,1)).toEqual(1);
     expect(component.subtracao(38736,36)).toEqual(38700);
-  });
-  it("verifica a exceção do método de subtração", ()=>{
-    expect(component.subtracao(2,1)).toEqual(1);
     expect(component.subtracao(0,1)).toEqual(-1);
     expect(component.subtracao(0,1)).not.toEqual(1);
-
+    expect(component.subtracao(1.25,1)).not.toEqual(1);
   });
   it("verifica o método de multiplicação", ()=>{
     expect(component.multiplicacao(2,-2)).toEqual(-4);
     expect(component.multiplicacao(-1,-2)).toEqual(2);
     expect(component.multiplicacao(200,5)).toEqual(1000);
     expect(component.multiplicacao(73635,0)).toEqual(0);
-
   });
   it("verifica o método divisão", ()=>{
     expect(component.divisao(8,4)).toEqual(2);
     expect(component.divisao(12,6)).toEqual(2);
     expect(component.divisao(14,-7)).not.toEqual(2);
     expect(component.divisao(0,6)).toBeNull;
-
   });
-  
   it("verifica a exceção do método divisão", ()=>{
     expect(function(){component.divisao(1,0)}).toThrow();
     expect(function(){component.divisao(468374,0)}).toThrow();
@@ -53,5 +49,8 @@ describe('CalculaComponent', () => {
   it("verifica o método raiz quadrada", ()=>{
     expect(component.raizQuadrada(9)).toEqual(3);
     expect(component.raizQuadrada(144)).toEqual(12);
+    expect(component.raizQuadrada(-144)).not.toEqual(12);
+    expect(component.raizQuadrada(374665)).not.toEqual(1);
+
   });
 });
